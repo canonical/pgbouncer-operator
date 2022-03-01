1dbbdfbb180b8cfc148393eed01d7a2828be629a
WRFitch <will.fitch@canonical.com>
2022-03-01 16:47:36 +0100

    updated & tested ini generation

--- lib/charms/dp_pgbouncer_operator/v0/pgb.py
+++ lib/charms/dp_pgbouncer_operator/v0/pgb.py
@@ -18,10 +18,11 @@ This charm library provides common pgbouncer-specific features for the pgbouncer
 Kubernetes charms.
 """
 
-from configparser import ConfigParser
+import io
 import logging
 import secrets
 import string
+from configparser import ConfigParser
 from typing import Dict
 
 from ops.model import ConfigData
@@ -101,7 +102,7 @@ def parse_userlist(userlist: str) -> Dict[str, str]:
     """
     parsed_userlist = {}
     for line in userlist.split("\n"):
-        # TODO add code that tests that double quotes wrap each value 
+        # TODO add code that tests that double quotes wrap each value
         if line.strip() == "" or len(line.split(" ")) != 2:
             logger.warning("unable to parse line in userlist file - user not imported")
             continue
@@ -112,91 +113,97 @@ def parse_userlist(userlist: str) -> Dict[str, str]:
     return parsed_userlist
 
 
-class PgbIniParser(ConfigParser): 
-    """
-    A ConfigParser class used to read, write, and edit pgbouncer.ini config files.
-    """
+class IniParser(ConfigParser):
+    """A ConfigParser class used to read, write, and edit pgbouncer.ini config files."""
+
     db_section = "databases"
     pgb_section = "pgbouncer"
-    list_values = ["admin_users", "stats_users"]
+    user_types = ["admin_users", "stats_users"]
 
-    def __init__(self):
-        super.__init__()
-        # Preserve case in string values 
+    def __init__(self, *args):
+        super().__init__(*args)
+        # Preserve case in string values
         self.optionxform = str
 
     def _read(self, fp, fpname):
-        """Overrides ConfigParser._read() method to include pgb-specific parsing of nested values.
+        """Reads a pgbouncer.ini file, and uses it to populate this object.
 
-        The existing ConfigParser._read() method adequately parses the config, but it's useful for 
-        us to parse dictionary and list values (such as databases and admin_users, respectively) 
-        explicitly, so we can access and modify those values as we need. This necessitates 
-        modifying the parent method's write() function to correctly encode these lists.
+        Overrides ConfigParser._read() method to include pgbouncer-specific parsing of nested
+        values. This method therefore also removes comments from the .ini file.
 
-        Args: 
-            fp: TODO
-            fpname: TODO
+        The existing ConfigParser._read() method adequately parses the config, but it's useful for
+        us to parse dictionary and list values (such as databases and admin_users, respectively)
+        explicitly, so we can access and modify those values as we need. This necessitates
+        modifying the parent method's write() function to correctly encode these lists. This method
+        is private, since the parent class' read methods should be used.
+
+        Args:
+            fp: filepath to be read. Must be iterable.
+            fpname: Source of filepath - for example, <String>
         """
-        super._read(fp, fpname)
-        # Parse nested dbs from space-separated key=value pairs to a dict. 
-        for _, db_config in self[db_section].items():
+        super()._read(fp, fpname)
+
+        # Parse db and user values into more useful python datatypes
+        self.dbs = {}
+        self.users = {}
+
+        # Parse nested dbs from space-separated key=value pairs to a dict.
+        for name, db_config in self[IniParser.db_section].items():
             db_config_dict = {}
-            for pair in dbconfig.split(" "):
+            for pair in db_config.split(" "):
                 key, value = pair.split("=")
                 db_config_dict[key] = value
-            db_config = db_config_dict
+            self.dbs[name] = db_config_dict
 
-        # Parse user lists from comma-separated strings to python lists. 
-        for value in list_values:
-            ls = self[pgb_section][value]
-            if ls and len(ls) > 1:
-                ls = ls.split(",")
+        # Parse user lists from comma-separated strings to python lists.
+        for user in IniParser.user_types:
+            try:
+                userlist = self[IniParser.pgb_section][user]
+                self.users[user] = userlist.split(",")
+            except KeyError:
+                # This user field doesn't exist, carry on.
+                continue
 
     def write(self, fp, space_around_delimiter=True):
-        """Overrides ConfigParser.write() method to include pgb-specific parsing of nested values.
-        
-        The existing ConfigParser.write() method adequately encodes simple config values, but 
+        """Write a pgbouncer file to the given filepath.
+
+        Overrides ConfigParser.write() method to include pgb-specific parsing of nested values.
+
+        The existing ConfigParser.write() method adequately encodes simple config values, but
         certain values are parsed into python lists/dicts in Ini._read(), for utility. These values
-        have to be re-encoded back to their string representations before they are written to a 
-        file. 
+        have to be re-encoded back to their string representations before they are written to a
+        file.
 
-        Args: 
-            fp: TODO
-            space_around_delimiter: TODO
+        Args:
+            fp: Iterable filepath object for
+            space_around_delimiter: whether or not to have spaces around delimiter
         """
-        # Store existing parsed values
-        parsed_db = self[db_section].copy()
-        admin_users = self[pgb_section]["admin_users"].copy()
-        stats_users = self[pgb_section]["stats_users"].copy()
-
         # Encode db dicts back to writable strings
-        for name, dbconfig in self[db_section].items():
+        for name, dbconfig in self.dbs.items():
             # Split each dbconfig value into a space-separated string of key-value pairs
-            self[db_section][name] = " ".join([f"{key}={value}" for key, value in dbconfig.items()])
+            self[IniParser.db_section][name] = " ".join(
+                [f"{key}={value}" for key, value in dbconfig.items()]
+            )
 
         # Encode user lists back to writable strings
-        for value in list_values:
-            self[pgb_section][value] = ",".join(self[pgb_section][value])
+        for userlist in IniParser.user_types:
+            try:
+                self[IniParser.pgb_section][userlist] = ",".join(self.users[userlist])
+            except KeyError:
+                # If admin user doesn't exist, skip
+                continue
 
         # Use ConfigParser.write() to write the file normally.
-        super.write(fp, space_around_delimiter)
-
-        # reinstate parsed values.
-        self.[db_section] = parsed_db
-        self[pgb_section]["admin_users"] = admin_users
-        self[pgb_section]["stats_users"] = stats_users
+        super().write(fp, space_around_delimiter)
 
     def write_to_string(self) -> str:
         """Writes class to a string, capable of being written to a pgbouncer.ini config file.
 
-        This uses a weird hack to return a string value from ConfigParser, where we write the 
-        config to a StringIO object, then immediately read from that object to retrieve a usable 
-        string. Removing this hack means entirely re-implementing ConfigParser.write(). 
-
         Returns:
             str: a string that can be sent to a pgbouncer.ini file.
         """
-        with io.StringIO() as string_out:
-            self.write(string_out)
-            write = string_out.seek(0).read()
-        return write
+        with io.StringIO() as string_io:
+            self.write(string_io)
+            string_io.seek(0)
+            rtn_string = string_io.read()
+        return rtn_string
--- tests/unit/test_pgb.py
+++ tests/unit/test_pgb.py
@@ -1,7 +1,6 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-
 import string
 import unittest
 
@@ -19,8 +18,7 @@ class TestPgb(unittest.TestCase):
     def test_generate_pgbouncer_ini(self):
         users = {"test1": "pw1", "test2": "pw2"}
         # Though this isn't correctly mocking an ops.model.ConfigData object, ConfigData implements
-        # a LazyMapping under the hood that accesses variables in the same way as a dictionary -
-        # they're effectively interchangeable in this context.
+        # a LazyMapping under the hood that accesses variables in the same way as a dictionary.
         config = {
             "pgb_databases": "test-dbs",
             "pgb_listen_port": "4454",
@@ -45,17 +43,76 @@ class TestPgb(unittest.TestCase):
     def test_parse_userlist(self):
         with open("tests/unit/data/test_userlist.txt") as f:
             userlist = f.read()
-            users = pgb.parse_userlist(userlist)
-            expected_users = {
-                "testuser": "testpass",
-                "another_testuser": "anotherpass",
-                "1234": "",
-                "": "",
-            }
-            self.assertDictEqual(users, expected_users)
+        users = pgb.parse_userlist(userlist)
+        expected_users = {
+            "testuser": "testpass",
+            "another_testuser": "anotherpass",
+            "1234": "",
+            "": "",
+        }
+        self.assertDictEqual(users, expected_users)
 
-            # Check that we can run input through a few times without anything getting corrupted.
-            regen_userlist = pgb.generate_userlist(users)
-            regen_users = pgb.parse_userlist(regen_userlist)
-            self.assertNotEqual(regen_userlist, userlist)
-            self.assertDictEqual(users, regen_users)
+        # Check that we can run input through a few times without anything getting corrupted.
+        regen_userlist = pgb.generate_userlist(users)
+        regen_users = pgb.parse_userlist(regen_userlist)
+        self.assertNotEqual(regen_userlist, userlist)
+        self.assertDictEqual(users, regen_users)
+
+    def test_pgb_ini_parser__read(self):
+        test_file = "tests/unit/data/test.ini"
+        parser = pgb.IniParser()
+
+        with open(test_file) as file:
+            parser._read(file, "testfile")
+
+        expected_parser_dbs = {
+            "db": {
+                "dbname": "test_db",
+                "host": "test",
+                "port": "10",
+                "user": "test",
+                "password": "test",
+                "client_encoding": "test",
+                "datestyle": "test",
+                "timezone": "test",
+                "pool_size": "test",
+                "connect_query": "test",
+            }
+        }
+        self.assertDictEqual(parser.dbs, expected_parser_dbs)
+        expected_users = {"admin_users": ["Test"], "stats_users": ["Test", "stats_test"]}
+        self.assertDictEqual(parser.users, expected_users)
+
+        parser_output = parser.write_to_string()
+
+        unmodded_file = ""
+        with open(test_file) as f:
+            for line in f.readlines():
+                # Remove commented lines
+                if line[0] not in ("#", ";"):
+                    unmodded_file += line
+
+        self.assertEqual(parser_output, unmodded_file)
+
+    def test_pgb_ini_parser_case_sensitive(self):
+        parser = pgb.IniParser()
+        self.assertEqual(parser.optionxform, str)
+        parser.read_dict({"pgbouncer": {"admin_users": "CAPSTEST"}})
+        self.assertEqual(parser["pgbouncer"]["admin_users"], "CAPSTEST")
+
+    def test_pgb_ini_parser_write_to_string(self):
+        test_file = "tests/unit/data/test.ini"
+        parser = pgb.IniParser()
+
+        with open(test_file) as file:
+            parser.read_file(file)
+        parser_output = parser.write_to_string()
+
+        unmodded_file = ""
+        with open(test_file) as f:
+            for line in f.readlines():
+                # Remove commented lines
+                if line[0] not in ("#", ";"):
+                    unmodded_file += line
+
+        self.assertEqual(parser_output, unmodded_file)
