33d6245ea48914cdfcc18f1a9ac17b07a0c66d02
Will Fitch <will.fitch@canonical.com>
2022-05-13 13:31:10 +0100

    DPE-151 Multiple pgbouncer systemd instances (#8)
    ## Issue
    Pgbouncer is single-threaded, so to make use of multiple cores we need to have multiple instances managed by systemd. 
    JIRA: [DPE-151](https://warthogs.atlassian.net/browse/DPE-151?atlOrigin=eyJpIjoiMmU3MDA0ODk3MmNkNGIyZjk1YTcwZDdhMGQ3MTM4OTgiLCJwIjoiaiJ9)
    
    ## Solution
    The charm now counts the available cores on the host unit and generates a separate directory for each instance containing a unique pgbouncer.ini config file, logfile, pidfile, and unix socket. These are derived from a "primary config", which is generic across all instances and is stored in the pgbouncer directory at /var/lib/postgres/pgbouncer. These services share the same `listen_port` value (6432, the default value for pgbouncer) by using `so_reuseport`, which allows pgbouncer to reuse the same unix port.
    
    A [slightly cleaner solution](https://www.2ndquadrant.com/en/blog/running-multiple-pgbouncer-instances-with-systemd/) exists for this problem, which makes use of systemd socket activation. This requires pgbouncer v1.14, which is unavailable on focal 20.04. Managing multiple instances automatically through the charm is messier in terms of charm code, but I feel it's cleaner overall than backporting or packaging pgbouncer for the sake of this functionality on one version of the machine charm. This solution is available on jammy 22.04.
    
    ## Release Notes
    - Implemented multiple pgbouncer services based on CPU cores available on each charm unit.
      - Templated pgbouncer.service (now renamed pgbouncer@.service).
      - Added multiple auto-generated pgbouncer.ini config files, and corresponding unique directories for each instance. 
    - PgbConfig object now has an option in the constructor to copy from other PgbConfig objects. 
    - Added new `website`, `source`, and `issues` sections to metadata.yaml.
    - Added `Raises` sections to docstrings.
    
    ## Testing
    - Added and updated relevant unit tests
    - Updated integration tests to verify the required number of pgbouncer services are running, and are correctly reset when they're killed. 
    
    
    * multiple instances working on focal
    
    * fixing tests and linter
    
    * fixed up unit tests
    
    * fixing a unit test mistake
    
    * fixing up some integration tests
    
    * fixed integration test and removed unnecessary service
    
    * fixing non-portable tests
    
    * cleaned up an integration test
    
    * resolving PR feedback
    
    Including integration test helpers and updating errors handled when installing apt packages
    
    * added basic method to get port ranges
    
    * fixed lint
    
    * updated based on PR feedback:
    
    Added better docs & comments
    renamed methods to make more sense
    
    * moving constant
    
    * fixed poor naming convention
    
    removed unnecessary port stuff

--- src/charm.py
+++ src/charm.py
@@ -26,6 +26,7 @@ PG_USER = "postgres"
 PGB_DIR = "/var/lib/postgresql/pgbouncer"
 INI_PATH = f"{PGB_DIR}/pgbouncer.ini"
 USERLIST_PATH = f"{PGB_DIR}/userlist.txt"
+INSTANCE_PATH = f"{PGB_DIR}/instance_"
 
 
 class PgBouncerCharm(CharmBase):
@@ -41,6 +42,10 @@ class PgBouncerCharm(CharmBase):
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.update_status, self._on_update_status)
 
+        self._cores = os.cpu_count()
+        self.service_ids = [service_id for service_id in range(self._cores)]
+        self.pgb_services = [f"{PGB}@{service_id}" for service_id in self.service_ids]
+
     # =======================
     #  Charm Lifecycle Hooks
     # =======================
@@ -52,23 +57,32 @@ class PgBouncerCharm(CharmBase):
         """
         self.unit.status = MaintenanceStatus("Installing and configuring PgBouncer")
 
-        # Initialise prereqs to run pgbouncer
         self._install_apt_packages([PGB])
 
-        os.mkdir(PGB_DIR, 0o600)
-
         pg_user = pwd.getpwnam(PG_USER)
+        os.mkdir(PGB_DIR, 0o777)
         os.chown(PGB_DIR, pg_user.pw_uid, pg_user.pw_gid)
 
-        # Initialise pgbouncer.ini config file from defaults set in charm lib.
+        # Make a directory for each service to store logs, configs, pidfiles and sockets.
+        # TODO this can be removed once socket activation is implemented (JIRA-218)
+        for service_id in self.service_ids:
+            os.mkdir(f"{INSTANCE_PATH}{service_id}", 0o777)
+            os.chown(f"{INSTANCE_PATH}{service_id}", pg_user.pw_uid, pg_user.pw_gid)
+
+        # Initialise pgbouncer.ini config files from defaults set in charm lib.
         ini = pgb.PgbConfig(pgb.DEFAULT_CONFIG)
-        self._render_pgb_config(ini)
-        # Initialise userlist, generating passwords for initial users
+        self._render_service_configs(ini)
+
+        # Initialise userlist, generating passwords for initial users. All config files use the
+        # same userlist, so we only need one.
         self._render_userlist(pgb.initialise_userlist_from_ini(ini))
 
-        # Enable pgbouncer and reload systemd
-        shutil.copy("src/pgbouncer.service", "/etc/systemd/system/pgbouncer.service")
+        # Copy pgbouncer service file and reload systemd
+        shutil.copy("src/pgbouncer@.service", "/etc/systemd/system/pgbouncer@.service")
         systemd.daemon_reload()
+        # Apt package starts its own pgbouncer service. Disable this so we can start and control
+        # our own.
+        systemd.service_stop(PGB)
 
         self.unit.status = WaitingStatus("Waiting to start PgBouncer")
 
@@ -78,7 +92,10 @@ class PgBouncerCharm(CharmBase):
         Runs pgbouncer through systemd (configured in src/pgbouncer.service)
         """
         try:
-            systemd.service_start(PGB)
+            for service in self.pgb_services:
+                logger.info(f"starting {service}")
+                systemd.service_start(f"{service}")
+
             self.unit.status = ActiveStatus("pgbouncer started")
         except systemd.SystemdError as e:
             logger.error(e)
@@ -90,12 +107,13 @@ class PgBouncerCharm(CharmBase):
         Uses systemd status to verify pgbouncer is running.
         """
         try:
-            if systemd.service_running(PGB):
-                self.unit.status = ActiveStatus()
-            else:
-                self.unit.status = BlockedStatus(
-                    "pgbouncer is not running - try restarting using `juju actions pgbouncer restart`"
-                )
+            for service in self.pgb_services:
+                if not systemd.service_running(f"{service}"):
+                    self.unit.status = self.unit.status = BlockedStatus(
+                        f"{service} is not running - try restarting using `juju actions pgbouncer restart`"
+                    )
+                    return
+            self.unit.status = ActiveStatus()
         except systemd.SystemdError as e:
             logger.error(e)
             self.unit.status = BlockedStatus("failed to get pgbouncer status")
@@ -110,52 +128,14 @@ class PgBouncerCharm(CharmBase):
 
         config.set_max_db_connection_derivatives(
             self.config["max_db_connections"],
-            os.cpu_count(),
+            self._cores,
         )
 
-        self._render_pgb_config(config, reload_pgbouncer=True)
+        self._render_service_configs(config, reload_pgbouncer=True)
 
-    # ==========================
-    #  Generic Helper Functions
-    # ==========================
-
-    def _install_apt_packages(self, packages: List[str]):
-        """Simple wrapper around 'apt-get install -y."""
-        try:
-            logger.debug("updating apt cache")
-            apt.update()
-        except subprocess.CalledProcessError as e:
-            logger.exception("failed to update apt cache, CalledProcessError", exc_info=e)
-            self.unit.status = BlockedStatus("failed to update apt cache")
-            return
-
-        try:
-            logger.debug(f"installing apt packages: {', '.join(packages)}")
-            apt.add_package(packages)
-        except apt.PackageNotFoundError as e:
-            logger.error(e)
-            self.unit.status = BlockedStatus("failed to install packages")
-
-    def _render_file(self, path: str, content: str, mode: int) -> None:
-        """Write content rendered from a template to a file.
-
-        Args:
-            path: the path to the file.
-            content: the data to be written to the file.
-            mode: access permission mask applied to the file using chmod (e.g. 0o640).
-        """
-        with open(path, "w+") as file:
-            file.write(content)
-        # Ensure correct permissions are set on the file.
-        os.chmod(path, mode)
-        # Get the uid/gid for the pgbouncer user.
-        u = pwd.getpwnam(PG_USER)
-        # Set the correct ownership for the file.
-        os.chown(path, uid=u.pw_uid, gid=u.pw_gid)
-
-    # =====================================
-    #  PgBouncer-Specific Helper Functions
-    # =====================================
+    # ==============================
+    #  PgBouncer-Specific Utilities
+    # ==============================
 
     def _read_pgb_config(self) -> pgb.PgbConfig:
         """Get config object from pgbouncer.ini file.
@@ -167,8 +147,39 @@ class PgBouncerCharm(CharmBase):
             config = pgb.PgbConfig(file.read())
         return config
 
+    def _render_service_configs(self, primary_config: pgb.PgbConfig, reload_pgbouncer=False):
+        """Derives config files for the number of required services from given config.
+
+        This method takes a primary config and generates one unique config for each intended
+        instance of pgbouncer, implemented as a templated systemd service.
+
+        TODO JIRA-218: Once pgbouncer v1.14 is available, update to use socket activation:
+        TODO https://warthogs.atlassian.net/browse/DPE-218
+        """
+        self.unit.status = MaintenanceStatus("updating PgBouncer config")
+
+        # Render primary config. This config is the only copy that the charm reads from to create
+        # PgbConfig objects, and is modified below to implement individual services.
+        self._render_pgb_config(pgb.PgbConfig(primary_config), config_path=INI_PATH)
+
+        # Modify & render config files for each service instance
+        for service_id in self.service_ids:
+            instance_dir = f"{INSTANCE_PATH}{service_id}"  # Generated in on_install hook
+
+            primary_config[PGB]["unix_socket_dir"] = instance_dir
+            primary_config[PGB]["logfile"] = f"{instance_dir}/pgbouncer.log"
+            primary_config[PGB]["pidfile"] = f"{instance_dir}/pgbouncer.pid"
+
+            self._render_pgb_config(primary_config, config_path=f"{instance_dir}/pgbouncer.ini")
+
+        if reload_pgbouncer:
+            self._reload_pgbouncer()
+
     def _render_pgb_config(
-        self, pgbouncer_ini: pgb.PgbConfig, reload_pgbouncer: bool = False
+        self,
+        pgbouncer_ini: pgb.PgbConfig,
+        reload_pgbouncer: bool = False,
+        config_path: str = INI_PATH,
     ) -> None:
         """Render config object to pgbouncer.ini file.
 
@@ -178,9 +189,10 @@ class PgBouncerCharm(CharmBase):
                 in the container. When config files are updated, pgbouncer must be restarted for
                 the changes to take effect. However, these config updates can be done in batches,
                 minimising the amount of necessary restarts.
+            config_path: intended location for the config.
         """
         self.unit.status = MaintenanceStatus("updating PgBouncer config")
-        self._render_file(INI_PATH, pgbouncer_ini.render(), 0o600)
+        self._render_file(config_path, pgbouncer_ini.render(), 0o777)
 
         if reload_pgbouncer:
             self._reload_pgbouncer()
@@ -201,7 +213,7 @@ class PgBouncerCharm(CharmBase):
                 minimising the amount of necessary restarts.
         """
         self.unit.status = MaintenanceStatus("updating PgBouncer users")
-        self._render_file(USERLIST_PATH, pgb.generate_userlist(userlist), 0o600)
+        self._render_file(USERLIST_PATH, pgb.generate_userlist(userlist), 0o777)
 
         if reload_pgbouncer:
             self._reload_pgbouncer()
@@ -210,11 +222,50 @@ class PgBouncerCharm(CharmBase):
         """Reloads systemd pgbouncer service."""
         self.unit.status = MaintenanceStatus("Reloading Pgbouncer")
         try:
-            systemd.service_reload(PGB, restart_on_failure=True)
+            for service in self.pgb_services:
+                systemd.service_reload(service, restart_on_failure=True)
             self.unit.status = ActiveStatus("PgBouncer Reloaded")
         except systemd.SystemdError as e:
             logger.error(e)
             self.unit.status = BlockedStatus("Failed to restart pgbouncer")
+
+    # =================
+    #  Charm Utilities
+    # =================
+
+    def _install_apt_packages(self, packages: List[str]):
+        """Simple wrapper around 'apt-get install -y."""
+        try:
+            logger.debug("updating apt cache")
+            apt.update()
+        except subprocess.CalledProcessError as e:
+            logger.exception("failed to update apt cache, CalledProcessError", exc_info=e)
+            self.unit.status = BlockedStatus("failed to update apt cache")
+            return
+
+        try:
+            logger.debug(f"installing apt packages: {', '.join(packages)}")
+            apt.add_package(packages)
+        except (apt.PackageNotFoundError, apt.PackageError, TypeError) as e:
+            logger.error(e)
+            self.unit.status = BlockedStatus("failed to install packages")
+
+    def _render_file(self, path: str, content: str, mode: int) -> None:
+        """Write content rendered from a template to a file.
+
+        Args:
+            path: the path to the file.
+            content: the data to be written to the file.
+            mode: access permission mask applied to the file using chmod (e.g. 0o640).
+        """
+        with open(path, "w+") as file:
+            file.write(content)
+        # Ensure correct permissions are set on the file.
+        os.chmod(path, mode)
+        # Get the uid/gid for the postgres user.
+        u = pwd.getpwnam(PG_USER)
+        # Set the correct ownership for the file.
+        os.chown(path, uid=u.pw_uid, gid=u.pw_gid)
 
 
 if __name__ == "__main__":
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,16 +1,17 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import os
 import subprocess
 import unittest
 from copy import deepcopy
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 import ops.testing
 from charms.operator_libs_linux.v0 import apt
 from charms.operator_libs_linux.v1 import systemd
 from charms.pgbouncer_operator.v0 import pgb
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
 from ops.testing import Harness
 
 from charm import PgBouncerCharm
@@ -32,78 +33,111 @@ class TestCharm(unittest.TestCase):
         self.harness.begin()
 
     @patch("charm.PgBouncerCharm._install_apt_packages")
+    @patch("charms.operator_libs_linux.v1.systemd.service_stop")
     @patch("os.mkdir")
     @patch("os.chown")
     @patch("pwd.getpwnam", return_value=MagicMock(pw_uid=1100, pw_gid=120))
     @patch("charm.PgBouncerCharm._render_file")
+    @patch("charm.PgBouncerCharm._render_service_configs")
     @patch("charms.pgbouncer_operator.v0.pgb.initialise_userlist_from_ini")
     @patch("shutil.copy")
     @patch("charms.operator_libs_linux.v1.systemd.daemon_reload")
     def test_on_install(
-        self, _reload, _copy, _userlist, _render_file, _getpwnam, _chown, _mkdir, _install
+        self,
+        _reload,
+        _copy,
+        _userlist,
+        _render_configs,
+        _render_file,
+        _getpwnam,
+        _chown,
+        _mkdir,
+        _stop,
+        _install,
     ):
         _userlist.return_value = {"juju-admin": "test"}
 
         self.harness.charm.on.install.emit()
 
         _install.assert_called_with(["pgbouncer"])
-
-        _mkdir.assert_called_once_with(PGB_DIR, 0o600)
+        _mkdir.assert_any_call(PGB_DIR, 0o777)
         _chown.assert_any_call(PGB_DIR, 1100, 120)
 
-        # Check config files are rendered correctly, including correct permissions
-        initial_pgbouncer_ini = pgb.PgbConfig(pgb.DEFAULT_CONFIG).render()
+        for service_id in self.harness.charm.service_ids:
+            _mkdir.assert_any_call(f"{PGB_DIR}/instance_{service_id}", 0o777)
+            _chown.assert_any_call(f"{PGB_DIR}/instance_{service_id}", 1100, 120)
+
+        # Check config files are rendered, including correct permissions
+        initial_cfg = pgb.PgbConfig(pgb.DEFAULT_CONFIG)
         initial_userlist = '"juju-admin" "test"'
-        _render_file.assert_any_call(INI_PATH, initial_pgbouncer_ini, 0o600)
-        _render_file.assert_any_call(USERLIST_PATH, initial_userlist, 0o600)
+        _render_configs.assert_called_once_with(initial_cfg)
+        _render_file.assert_any_call(USERLIST_PATH, initial_userlist, 0o777)
 
-        self.assertTrue(isinstance(self.harness.model.unit.status, WaitingStatus))
+        self.assertIsInstance(self.harness.model.unit.status, WaitingStatus)
 
-    @patch("charms.operator_libs_linux.v1.systemd.service_start")
+    @patch("charms.operator_libs_linux.v1.systemd.service_start", side_effect=systemd.SystemdError)
     def test_on_start(self, _start):
+        intended_instances = self._cores = os.cpu_count()
+        # Testing charm blocks when systemd is in error
         self.harness.charm.on.start.emit()
-        _start.assert_called_once_with("pgbouncer")
-        self.assertTrue(isinstance(self.harness.model.unit.status, ActiveStatus))
+        _start.assert_called()
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
 
-        _start.side_effect = systemd.SystemdError()
+        # Testing charm starts the correct amount of pgbouncer instances and enters activestatus if
+        # everything's working fine.
+        _start.side_effect = None
         self.harness.charm.on.start.emit()
-        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+        calls = [call(f"pgbouncer@{instance}") for instance in range(intended_instances)]
+        _start.assert_has_calls(calls)
+        self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
 
     @patch("charms.operator_libs_linux.v1.systemd.service_reload")
     def test_reload_pgbouncer(self, _reload):
+        intended_instances = self._cores = os.cpu_count()
         self.harness.charm._reload_pgbouncer()
-        _reload.assert_called_once_with("pgbouncer", restart_on_failure=True)
-        self.assertTrue(isinstance(self.harness.model.unit.status, ActiveStatus))
+        calls = [
+            call(f"pgbouncer@{instance}", restart_on_failure=True)
+            for instance in range(intended_instances)
+        ]
+        _reload.assert_has_calls(calls)
+        self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
 
+        # Verify that if systemd is in error, the charm enters blocked status.
         _reload.side_effect = systemd.SystemdError()
         self.harness.charm._reload_pgbouncer()
-        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
 
     @patch("charms.operator_libs_linux.v1.systemd.service_running", return_value=False)
     def test_on_update_status(self, _running):
+        intended_instances = self._cores = os.cpu_count()
+        # Testing charm blocks when the pgbouncer services aren't running
         self.harness.charm.on.update_status.emit()
-        _running.assert_called_once_with("pgbouncer")
-        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
 
+        # If all pgbouncer services are running, verify we enter an active status.
         _running.return_value = True
         self.harness.charm.on.update_status.emit()
-        self.assertTrue(isinstance(self.harness.model.unit.status, ActiveStatus))
+        calls = [call(f"pgbouncer@{instance}") for instance in range(intended_instances)]
+        _running.assert_has_calls(calls)
+        self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
 
         _running.side_effect = systemd.SystemdError()
         self.harness.charm.on.update_status.emit()
-        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
 
     @patch("charm.PgBouncerCharm._read_pgb_config", return_value=pgb.PgbConfig(pgb.DEFAULT_CONFIG))
-    @patch("charm.PgBouncerCharm._render_pgb_config")
-    @patch("os.cpu_count", return_value=1)
-    def test_on_config_changed(self, _cpu_count, _render, _read):
+    @patch("charm.PgBouncerCharm._render_service_configs")
+    def test_on_config_changed(self, _render, _read):
+        mock_cores = 1
+        self.harness.charm._cores = mock_cores
         max_db_connections = 44
+
         # Copy config object and modify it as we expect in the hook.
         config = deepcopy(_read.return_value)
         config["pgbouncer"]["pool_mode"] = "transaction"
         config.set_max_db_connection_derivatives(
             max_db_connections=max_db_connections,
-            pgb_instances=_cpu_count.return_value,
+            pgb_instances=mock_cores,
         )
 
         # set config to include pool_mode and max_db_connections
@@ -171,7 +205,7 @@ class TestCharm(unittest.TestCase):
             test_config = pgb.PgbConfig(ini.read())
 
         self.harness.charm._render_pgb_config(test_config, reload_pgbouncer=False)
-        _render.assert_called_with(INI_PATH, test_config.render(), 0o600)
+        _render.assert_called_with(INI_PATH, test_config.render(), 0o777)
         _reload.assert_not_called()
 
         # Copy config and edit a value
@@ -179,8 +213,41 @@ class TestCharm(unittest.TestCase):
         reload_config["pgbouncer"]["admin_users"] = ["test_admin"]
 
         self.harness.charm._render_pgb_config(reload_config, reload_pgbouncer=True)
-        _render.assert_called_with(INI_PATH, reload_config.render(), 0o600)
+        _render.assert_called_with(INI_PATH, reload_config.render(), 0o777)
         _reload.assert_called()
+
+        self.harness.charm._render_pgb_config(reload_config, config_path="/test/path")
+        _render.assert_called_with("/test/path", reload_config.render(), 0o777)
+
+    @patch("charm.PgBouncerCharm._reload_pgbouncer")
+    @patch("charm.PgBouncerCharm._render_file")
+    def test_render_service_configs(self, _render, _reload):
+        self.harness.charm.service_ids = [0, 1]
+        default_cfg = pgb.PgbConfig(pgb.DEFAULT_CONFIG)
+        cfg_list = [default_cfg.render()]
+
+        for service_id in self.harness.charm.service_ids:
+            cfg = pgb.PgbConfig(pgb.DEFAULT_CONFIG)
+            instance_dir = f"{PGB_DIR}/instance_{service_id}"
+
+            cfg["pgbouncer"]["unix_socket_dir"] = instance_dir
+            cfg["pgbouncer"]["logfile"] = f"{instance_dir}/pgbouncer.log"
+            cfg["pgbouncer"]["pidfile"] = f"{instance_dir}/pgbouncer.pid"
+
+            cfg_list.append(cfg.render())
+
+        self.harness.charm._render_service_configs(default_cfg, reload_pgbouncer=False)
+
+        _render.assert_any_call(INI_PATH, cfg_list[0], 0o777)
+        _render.assert_any_call(f"{PGB_DIR}/instance_0/pgbouncer.ini", cfg_list[1], 0o777)
+        _render.assert_any_call(f"{PGB_DIR}/instance_1/pgbouncer.ini", cfg_list[2], 0o777)
+
+        _reload.assert_not_called()
+        # MaintenanceStatus will exit once pgbouncer reloads.
+        self.assertIsInstance(self.harness.model.unit.status, MaintenanceStatus)
+
+        self.harness.charm._render_service_configs(cfg, reload_pgbouncer=True)
+        _reload.assert_called_once()
 
     def test_read_userlist(self):
         test_users = {"test_user": "test_pass"}
@@ -196,10 +263,10 @@ class TestCharm(unittest.TestCase):
         test_users = {"test_user": "test_pass"}
 
         self.harness.charm._render_userlist(test_users, reload_pgbouncer=False)
-        _render.assert_called_with(USERLIST_PATH, pgb.generate_userlist(test_users), 0o600)
+        _render.assert_called_with(USERLIST_PATH, pgb.generate_userlist(test_users), 0o777)
         _reload.assert_not_called()
 
         reload_users = {"reload_user": "reload_pass"}
         self.harness.charm._render_userlist(reload_users, reload_pgbouncer=True)
-        _render.assert_called_with(USERLIST_PATH, pgb.generate_userlist(reload_users), 0o600)
+        _render.assert_called_with(USERLIST_PATH, pgb.generate_userlist(reload_users), 0o777)
         _reload.assert_called()
