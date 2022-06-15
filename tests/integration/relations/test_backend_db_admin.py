2c9d552bfbdd10e8ba0ec559ade689317a9b8695
WRFitch <will.fitch@canonical.com>
2022-06-15 17:24:02 +0100

    fixed integration test, and hopefully expedited it

--- tests/integration/relations/test_backend_db_admin.py
+++ tests/integration/relations/test_backend_db_admin.py
@@ -1,6 +1,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import asyncio
 import logging
 from pathlib import Path
 
@@ -18,15 +19,25 @@ APP_NAME = METADATA["name"]
 # TODO test we can actually send and receive data through it
 
 
-async def test_backend_db_admin_relation_slowtest(ops_test: OpsTest):
+async def test_backend_db_admin_relation_slowtest_current(ops_test: OpsTest):
     """Test that the pgbouncer and postgres charms can relate to one another.
 
     This test is marked "slowtest", meaning it won't run when you run "tox -e fast-integration".
     This is because it's very slow.
     """
+    # Build, deploy, and relate charms.
     pg = "postgresql"
-    await ops_test.model.deploy(pg)
+    charm = await ops_test.build_charm(".")
+    await asyncio.join(ops_test.model.deploy(
+            charm,
+            application_name=APP_NAME,
+        ),
+        ops_test.model.deploy(pg)
+    )
+    # Pgbouncer enters a waiting state without backend postgres relation
+    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="waiting", timeout=1000)
     await ops_test.model.add_relation(f"{APP_NAME}:backend-db-admin", f"{pg}:db-admin")
+    # Pgbouncer enters a waiting status without a postgres backend database relation
     await ops_test.model.wait_for_idle(apps=[APP_NAME, pg], status="active", timeout=1000)
 
     unit = ops_test.model.units["pgbouncer-operator/0"]
@@ -35,8 +46,13 @@ async def test_backend_db_admin_relation_slowtest(ops_test: OpsTest):
     assert "pg_master" in cfg["databases"].keys()
 
     await ops_test.model.applications[pg].add_units(count=2)
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, pg], status="active", timeout=1000, wait_for_exact_units=3
+    await asyncio.join(
+        ops_test.model.wait_for_idle(
+            apps=[pg], status="active", timeout=1000, wait_for_exact_units=3
+        ),
+        ops_test.model.wait_for_idle(
+            apps=[APP_NAME], status="active", timeout=1000, wait_for_exact_units=1
+        ),
     )
     cfg = await helpers.get_cfg(unit)
     # Now there are three postgres units, we're in "standby" mode, with two standby replicas.
@@ -45,8 +61,13 @@ async def test_backend_db_admin_relation_slowtest(ops_test: OpsTest):
     ].keys()
 
     await ops_test.model.applications[pg].scale(2)
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, pg], status="active", timeout=1000, wait_for_exact_units=2
+    await asyncio.join(
+        ops_test.model.wait_for_idle(
+            apps=[pg], status="active", timeout=1000, wait_for_exact_units=2
+        ),
+        ops_test.model.wait_for_idle(
+            apps=[APP_NAME], status="active", timeout=1000, wait_for_exact_units=1
+        ),
     )
     cfg = await helpers.get_cfg(unit)
     # Now there are two postgres units, and the config reflects this.
@@ -55,7 +76,7 @@ async def test_backend_db_admin_relation_slowtest(ops_test: OpsTest):
 
     await ops_test.model.applications[pg].scale(1)
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, pg], status="active", timeout=1000, wait_for_exact_units=1
+        apps=[APP_NAME, pg], status="active", timeout=1000
     )
     cfg = await helpers.get_cfg(unit)
     # Now there is only one config, with no replicas, and the config reflects this.
