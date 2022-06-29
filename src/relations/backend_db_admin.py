94174739af279b119e90939bdef4bb8bdc3b8183
WRFitch <will.fitch@canonical.com>
2022-06-29 14:29:47 +0100

    removed commented code

--- src/relations/backend_db_admin.py
+++ src/relations/backend_db_admin.py
@@ -113,9 +113,6 @@ class BackendDbAdminRequires(Object):
         )
 
         postgres_data = change_event.relation.data.get(change_event.unit)
-        # if not postgres_data:
-        #     # if change_event.unit doesn't exist,
-        #     return
 
         cfg = self.charm._read_pgb_config()
         dbs = cfg["databases"]
