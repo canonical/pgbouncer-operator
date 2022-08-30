cb5fb9fee0ee517171cb475601f064220a73da08
WRFitch <will.fitch@canonical.com>
2022-08-30 15:11:10 +0100

    initial file adding

---
+++ tests/integration/relations/test_backend_database.py
@@ -0,0 +1,91 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import asyncio
+import logging
+from pathlib import Path
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+from tenacity import RetryError, Retrying, stop_after_delay, wait_fixed
+
+from tests.integration.helpers.helpers import (
+    get_app_relation_databag,
+    get_backend_user_pass,
+    get_cfg,
+    get_pgb_log,
+    get_userlist,
+    wait_for_relation_joined_between,
+    wait_for_relation_removed_between,
+)
+from tests.integration.helpers.postgresql_helpers import check_database_users_existence
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+PGB = METADATA["name"]
+PG = "postgresql-k8s"
+RELATION = "backend-database"
+
+
+@pytest.mark.skip
+@pytest.mark.backend
+async def test_relate_pgbouncer_to_postgres(ops_test: OpsTest):
+    """Test that the pgbouncer and postgres charms can relate to one another."""
+    # Build, deploy, and relate charms.
+    charm = await ops_test.build_charm(".")
+    async with ops_test.fast_forward():
+        await asyncio.gather(
+            ops_test.model.deploy(
+                charm,
+                application_name=PGB,
+            ),
+            # Edge 5 is the new postgres charm
+            ops_test.model.deploy(PG, channel="edge", trust=True, num_units=3),
+        )
+        await asyncio.gather(
+            ops_test.model.wait_for_idle(apps=[PGB], status="active", timeout=1000),
+            ops_test.model.wait_for_idle(
+                apps=[PG], status="active", timeout=1000, wait_for_exact_units=3
+            ),
+        )
+
+        relation = await ops_test.model.add_relation(f"{PGB}:{RELATION}", f"{PG}:database")
+        wait_for_relation_joined_between(ops_test, PG, PGB)
+        await ops_test.model.wait_for_idle(apps=[PGB, PG], status="active", timeout=1000),
+
+        cfg = await get_cfg(ops_test)
+        logging.info(cfg.render())
+        pgb_user, pgb_password = await get_backend_user_pass(ops_test, relation)
+        assert pgb_user in cfg["pgbouncer"]["admin_users"]
+        assert cfg["pgbouncer"]["auth_query"]
+
+        await check_database_users_existence(ops_test, [pgb_user], [], pgb_user, pgb_password)
+
+        # Remove relation but keep pg application because we're going to need it for future tests.
+        await ops_test.model.applications[PG].remove_relation(
+            f"{PGB}:{RELATION}", f"{PG}:database"
+        )
+        pgb_unit = ops_test.model.applications[PGB].units[0]
+        logging.info(await get_app_relation_databag(ops_test, pgb_unit.name, relation.id))
+        wait_for_relation_removed_between(ops_test, PG, PGB)
+        await ops_test.model.wait_for_idle(apps=[PG, PGB], status="active", timeout=1000),
+
+        # Wait for pgbouncer charm to update its config files.
+        try:
+            for attempt in Retrying(stop=stop_after_delay(3 * 60), wait=wait_fixed(3)):
+                with attempt:
+                    cfg = await get_cfg(ops_test)
+                    if (
+                        pgb_user not in cfg["pgbouncer"]["admin_users"]
+                        and "auth_query" not in cfg["pgbouncer"].keys()
+                    ):
+                        break
+        except RetryError:
+            assert False, "pgbouncer config files failed to update in 3 minutes"
+
+        cfg = await get_cfg(ops_test)
+        logging.info(cfg.render())
+        logger.info(await get_pgb_log(ops_test))
+
