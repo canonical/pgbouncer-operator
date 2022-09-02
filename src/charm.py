f9fc43ef6e83a360cb1110ec3d96724514ba99c0
WRFitch <will.fitch@canonical.com>
2022-09-02 13:01:53 +0100

    updated comments to remove references to containers

--- src/charm.py
+++ src/charm.py
@@ -210,9 +210,9 @@ class PgBouncerCharm(CharmBase):
 
         Args:
             pgbouncer_ini: PgbConfig object containing pgbouncer config.
-            reload_pgbouncer: A boolean defining whether or not to reload the pgbouncer application
-                in the container. When config files are updated, pgbouncer must be restarted for
-                the changes to take effect. However, these config updates can be done in batches,
+            reload_pgbouncer: A boolean defining whether or not to reload the pgbouncer
+                application. When config files are updated, pgbouncer must be restarted for the
+                changes to take effect. However, these config updates can be done in batches,
                 minimising the amount of necessary restarts.
             config_path: intended location for the config.
         """
@@ -232,9 +232,9 @@ class PgBouncerCharm(CharmBase):
 
         Args:
             userlist: dictionary of users:password strings.
-            reload_pgbouncer: A boolean defining whether or not to reload the pgbouncer application
-                in the container. When config files are updated, pgbouncer must be restarted for
-                the changes to take effect. However, these config updates can be done in batches,
+            reload_pgbouncer: A boolean defining whether or not to reload the pgbouncer
+                application. When config files are updated, pgbouncer must be restarted for the
+                changes to take effect. However, these config updates can be done in batches,
                 minimising the amount of necessary restarts.
         """
         self.unit.status = MaintenanceStatus("updating PgBouncer users")
