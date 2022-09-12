aaea6210eb25fa3c2a8b85d21344d3415f5d0930
WRFitch <will.fitch@canonical.com>
2022-09-12 12:50:33 +0100

    added simple psql test in lieu of complex tests all being too complex to actually work

--- tests/integration/relations/test_db_admin.py
+++ tests/integration/relations/test_db_admin.py
@@ -12,8 +12,10 @@ from pytest_operator.plugin import OpsTest
 from tests.integration.helpers.helpers import (
     deploy_and_relate_application_with_pgbouncer_bundle,
     deploy_postgres_bundle,
+    get_app_relation_databag,
     get_backend_user_pass,
     get_legacy_relation_username,
+    get_unit_info,
     wait_for_relation_joined_between,
 )
 from tests.integration.helpers.postgresql_helpers import (
@@ -26,40 +28,30 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 PGB = METADATA["name"]
 PG = "postgresql"
-TELEGRAF = "telegraf"
-INFLUXDB = "influxdb"
+PSQL = "psql"
 
 
 @pytest.mark.legacy_relation
-async def test_db_admin_with_telegraf(ops_test: OpsTest) -> None:
-    """Deploy telegraf and influxdb to test the 'db-admin' relation."""
-    backend_relation = await deploy_postgres_bundle(ops_test, db_units=1)
-    async with ops_test.fast_forward():
-        # Deploy Telegraf
-        telegraf_relation = await deploy_and_relate_application_with_pgbouncer_bundle(
-            ops_test, TELEGRAF, relation="db-admin"
-        )
+async def test_db_admin_with_psql(ops_test: OpsTest) -> None:
+    pg_relation = await deploy_postgres_bundle(ops_test)
+    psql_relation = await deploy_and_relate_application_with_pgbouncer_bundle(
+        ops_test, "postgresql-charmers-postgresql-client", application_name=PSQL
+    )
+    unit_name = f"{PSQL}/0"
+    psql_databag = get_app_relation_databag(ops_test, unit_name, psql_relation.id)
 
-        await asyncio.gather(
-            ops_test.model.relate(TELEGRAF, f"{PGB}:juju-info"),
-            ops_test.model.deploy(INFLUXDB),
-        )
+    pgpass = psql_databag.get("password")
+    user = psql_databag.get("user")
+    host = psql_databag.get("host")
+    port = psql_databag.get("port")
+    dbname = psql_databag.get("database")
 
-        await ops_test.model.relate(TELEGRAF, INFLUXDB)
-        wait_for_relation_joined_between(ops_test, PGB, TELEGRAF)
-        wait_for_relation_joined_between(ops_test, INFLUXDB, TELEGRAF)
+    assert None in [pgpass,user,host,port,dbname], "databag incorrectly populated"
 
-        await ops_test.model.wait_for_idle(
-            apps=[TELEGRAF, INFLUXDB, PG, PGB],
-            status="active",
-            timeout=1000,
-        )
+    user_command = f"run --unit {unit_name} -- PGPASSWORD={pgpass} psql --username={user} -h {host} -p {port} --dbname={dbname} --command=\"CREATE ROLE myuser3 LOGIN PASSWORD 'mypass' ;\""
+    rtn, _, err = await ops_test.juju(*user_command.split(" "))
+    assert rtn == 0, f"failed to run admin command, {err}"
 
-        pgb_user, pgb_pass = await get_backend_user_pass(ops_test, backend_relation)
-        await check_databases_creation(ops_test, ["mailman3"], pgb_user, pgb_pass)
-
-        telegraf_users = get_legacy_relation_username(ops_test, telegraf_relation.id)
-
-        await check_database_users_existence(ops_test, [telegraf_users], [], pgb_user, pgb_pass)
-
-        # TODO assert telegraf is working
+    db_command = f"run --unit {unit_name} -- PGPASSWORD={pgpass} psql --username={user} -h {host} -p {port} --dbname={dbname} --command=\"CREATE DATABASE test_db;\""
+    rtn, _, err = await ops_test.juju(*db_command.split(" "))
+    assert rtn == 0, f"failed to run admin command, {err}"
