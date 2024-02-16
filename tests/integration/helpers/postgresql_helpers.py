2ee07df00288eda6ff4fd4a3ebeac974ad2bd1b7
Dragomir Penev <dragomir.penev@canonical.com>
2024-02-16 22:48:45 +0200

    Integration tests

--- tests/integration/helpers/postgresql_helpers.py
+++ tests/integration/helpers/postgresql_helpers.py
@@ -2,6 +2,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 import itertools
+import subprocess
 from typing import List
 
 import psycopg2
@@ -179,3 +180,49 @@ def get_unit_address(ops_test: OpsTest, unit_name: str) -> str:
         IP address of the unit
     """
     return ops_test.model.units.get(unit_name).public_address
+
+
+async def run_command_on_unit(ops_test: OpsTest, unit_name: str, command: str) -> str:
+    """Run a command on a specific unit.
+
+    Args:
+        ops_test: The ops test framework instance
+        unit_name: The name of the unit to run the command on
+        command: The command to run
+
+    Returns:
+        the command output if it succeeds, otherwise raises an exception.
+    """
+    complete_command = ["exec", "--unit", unit_name, "--", *command.split()]
+    return_code, stdout, _ = await ops_test.juju(*complete_command)
+    if return_code != 0:
+        raise Exception(
+            "Expected command %s to succeed instead it failed: %s", command, return_code
+        )
+    return stdout
+
+
+async def get_machine_from_unit(ops_test: OpsTest, unit_name: str) -> str:
+    """Get the name of the machine from a specific unit.
+
+    Args:
+        ops_test: The ops test framework instance
+        unit_name: The name of the unit to get the machine
+
+    Returns:
+        The name of the machine.
+    """
+    raw_hostname = await run_command_on_unit(ops_test, unit_name, "hostname")
+    return raw_hostname.strip()
+
+
+async def restart_machine(ops_test: OpsTest, unit_name: str) -> None:
+    """Restart the machine where a unit run on.
+
+    Args:
+        ops_test: The ops test framework instance
+        unit_name: The name of the unit to restart the machine
+    """
+    raw_hostname = await get_machine_from_unit(ops_test, unit_name)
+    restart_machine_command = f"lxc restart {raw_hostname}"
+    subprocess.check_call(restart_machine_command.split())
