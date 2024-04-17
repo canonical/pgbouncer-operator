68351a4a6c59d2b940037b1e04c0f887c1cf34ba
Dragomir Penev <dragomir.penev@canonical.com>
2024-04-17 13:42:49 +0300

    Use transaction pooling

--- tests/integration/relations/test_db_admin.py
+++ tests/integration/relations/test_db_admin.py
@@ -38,7 +38,7 @@ async def test_landscape_scalable_bundle_db(ops_test: OpsTest, pgb_charm_jammy: 
         db_units=DATABASE_UNITS,
         pgb_series="jammy",
         pg_config={"profile": "testing", "plugin_plpython3u_enable": "True"},
-        pgb_config={"max_db_connections": "500"},
+        pgb_config={"max_db_connections": "500", "pool_mode": "transaction"},
     )
 
     # Deploy and test the Landscape Scalable bundle (using this PostgreSQL charm).
