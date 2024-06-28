a95f13767a7774ef96f549ad2223d4f79493d152
Dragomir Penev <dragomir.penev@canonical.com>
2024-06-28 18:15:53 +0300

    Actually use the fault injection charm

--- tests/integration/test_upgrade.py
+++ tests/integration/test_upgrade.py
@@ -152,7 +152,7 @@ async def test_fail_and_rollback(ops_test, continuous_writes, pgb_charm_jammy) -
     application = ops_test.model.applications[PGB]
 
     logger.info("Refresh the charm")
-    await application.refresh(path=pgb_charm_jammy)
+    await application.refresh(path=fault_charm)
 
     logger.info("Wait for upgrade to fail")
     async with ops_test.fast_forward("60s"):
