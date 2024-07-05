eb94dddd12bd7c82e16828ff27209d95258eb1a3
Dragomir Penev <dragomir.penev@canonical.com>
2024-07-05 16:21:34 +0300

    Increate connections for landscape test

--- tests/integration/relations/test_db_admin.py
+++ tests/integration/relations/test_db_admin.py
@@ -38,7 +38,7 @@ async def test_landscape_scalable_bundle_db(ops_test: OpsTest, pgb_charm_jammy: 
         db_units=DATABASE_UNITS,
         pgb_series="jammy",
         pg_config={"profile": "testing"},
-        pgb_config={"max_db_connections": "20", "pool_mode": "transaction"},
+        pgb_config={"max_db_connections": "40", "pool_mode": "transaction"},
     )
 
     # Deploy and test the Landscape Scalable bundle (using this PostgreSQL charm).
