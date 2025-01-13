31f7e383c85e52ab1f8efc280a32ff3ac4198db6
Carl Csaposs <carl.csaposs@canonical.com>
2025-01-13 15:28:54 +0100

    Use absolute path

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -16,9 +16,16 @@ from .helpers.postgresql_helpers import get_leader_unit
 def pgb_charm_focal(ops_test: OpsTest):
     """Build the pgbouncer charm."""
     if architecture.architecture == "amd64":
-        return pathlib.Path("pgbouncer_ubuntu@20.04-amd64.charm")
+        # python-libjuju's model.deploy(), juju deploy, and juju bundle files expect local charms
+        # to begin with `./` or `/` to distinguish them from Charmhub charms.
+        # Therefore, we need to return an absolute path—a relative `pathlib.Path` does not start
+        # with `./` when cast to a str.
+        # (python-libjuju model.deploy() expects a str but will cast any input to a str as a
+        # workaround for pytest-operator's non-compliant `build_charm` return type of
+        # `pathlib.Path`.)
+        return pathlib.Path("pgbouncer_ubuntu@20.04-amd64.charm").resolve(strict=True)
     elif architecture.architecture == "arm64":
-        return pathlib.Path("pgbouncer_ubuntu@20.04-arm64.charm")
+        return pathlib.Path("pgbouncer_ubuntu@20.04-arm64.charm").resolve(strict=True)
     else:
         raise ValueError(architecture.architecture)
 
