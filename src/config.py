4e6daaffb987c6612c8980f6aa916ccc757c540e
Dragomir Penev <dragomir.penev@canonical.com>
2025-05-23 16:47:33 +0300

    Initial switch to tlsv4

--- src/config.py
+++ src/config.py
@@ -5,10 +5,10 @@
 """Structured configuration for the PostgreSQL charm."""
 
 import logging
-from typing import Literal, Optional
+from typing import Literal
 
-from charms.data_platform_libs.v0.data_models import BaseConfigModel
-from pydantic import IPvAnyAddress, PositiveInt, conint
+from charms.data_platform_libs.v1.data_models import BaseConfigModel
+from pydantic import Field, IPvAnyAddress
 
 logger = logging.getLogger(__name__)
 
@@ -16,9 +16,9 @@ logger = logging.getLogger(__name__)
 class CharmConfig(BaseConfigModel):
     """Manager for the structured configuration."""
 
-    listen_port: PositiveInt
-    metrics_port: PositiveInt
-    vip: Optional[IPvAnyAddress]
-    local_connection_type: Literal["tcp", "uds"]
-    pool_mode: Literal["session", "transaction", "statement"]
-    max_db_connections: conint(ge=0)
+    listen_port: int | None = Field(ge=1, default=6432)
+    metrics_port: int | None = Field(ge=1, default=9127)
+    vip: IPvAnyAddress | None = Field(default=None)
+    local_connection_type: Literal["tcp", "uds"] = Field(default="tcp")
+    pool_mode: Literal["session", "transaction", "statement"] = Field(default="session")
+    max_db_connections: int | None = Field(ge=0, default=100)
